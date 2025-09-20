7986cc03004a64edec68203f2a933a69fe75045b
felipe20092006 <inteligenciaartificial1air@gmail.com>
2025-09-19 22:26:51 -0300

    teste

---
+++ tipos.py
@@ -0,0 +1,19 @@
+var = 0.91
+var2 = 10.4
+var = 15.2
+
+nome = "Felipe" + "10"
+
+booleano = True
+booleano = False
+
+lista = ["Kiwi", "morango", "abacate"]
+lista2 = [1, 2, 3, 4, 5]
+
+
+
+
+resultado = var + var2
+print(resultado)
+
+print(nome)
